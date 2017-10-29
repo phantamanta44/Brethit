3e66dff345e3803e87fcde25b5292e91cf0b5445
phantamanta44 <evandalong@gmail.com>
2017-10-28 22:08:16 -0500

    Fix issue with eating itemstacks with only one item

--- src/main/java/io/github/phantamanta44/brethit/event/CommonEventHandler.java
+++ src/main/java/io/github/phantamanta44/brethit/event/CommonEventHandler.java
@@ -1,11 +1,17 @@
 package io.github.phantamanta44.brethit.event;
 
+import gnu.trove.map.TObjectIntMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.UUID;
+
 public class CommonEventHandler {
+
+    private final TObjectIntMap<UUID> eating = new TObjectIntHashMap<>();
 
     @SubscribeEvent
     public void onRegen(WorldEvent event) {
@@ -13,9 +19,24 @@ public class CommonEventHandler {
     }
 
     @SubscribeEvent
-    public void onEat(LivingEntityUseItemEvent.Finish event) {
-        if (event.getItem().getItem() instanceof ItemFood) {
-            event.getEntityLiving().heal(((ItemFood)event.getItem().getItem()).getHealAmount(event.getItem()));
+    public void onEatStart(LivingEntityUseItemEvent.Start event) {
+        if (!event.getEntityLiving().world.isRemote && event.getItem().getItem() instanceof ItemFood) {
+            eating.put(event.getEntityLiving().getUniqueID(), ((ItemFood)event.getItem().getItem()).getHealAmount(event.getItem()));
+        }
+    }
+    
+    @SubscribeEvent
+    public void onEatCancel(LivingEntityUseItemEvent.Stop event) {
+        if (!event.getEntityLiving().world.isRemote) {
+            eating.remove(event.getEntityLiving().getUniqueID());
+        }
+    }
+
+    @SubscribeEvent
+    public void onEatFinish(LivingEntityUseItemEvent.Finish event) {
+        if (!event.getEntityLiving().world.isRemote && eating.containsKey(event.getEntityLiving().getUniqueID())) {
+            event.getEntityLiving().heal(eating.get(event.getEntityLiving().getUniqueID()));
+            eating.remove(event.getEntityLiving().getUniqueID());
         }
     }
 
