315252e175066c565c2cafa08cf5647631ce1ac7
phantamanta44 <evandalong@gmail.com>
2017-11-22 01:08:35 -0600

    Make mantle foods always edible

--- src/main/java/io/github/phantamanta44/brethit/CommonProxy.java
+++ src/main/java/io/github/phantamanta44/brethit/CommonProxy.java
@@ -2,9 +2,14 @@ package io.github.phantamanta44.brethit;
 
 import io.github.phantamanta44.brethit.constant.LangConst;
 import io.github.phantamanta44.brethit.event.CommonEventHandler;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+
+import java.lang.reflect.Field;
+import java.util.BitSet;
 
 public class CommonProxy {
 
@@ -14,15 +19,34 @@ public class CommonProxy {
 
     public void onInit() {
         MinecraftForge.EVENT_BUS.register(new CommonEventHandler());
-        ForgeRegistries.ITEMS.forEach(i -> {
-            if (i instanceof ItemFood) {
-                ((ItemFood)i).setAlwaysEdible();
-            }
-        });
     }
 
     public void onPostInit() {
-        // NO-OP
+        boolean mantleLoaded = true;
+        Class<?> classItemEdible = null;
+        Field fieldAlwaysEdible = null;
+        try {
+            classItemEdible = Class.forName("slimeknights.mantle.item.ItemEdible");
+            fieldAlwaysEdible = ReflectionHelper.findField(classItemEdible, "field_77852_bZ", "alwaysEdible");
+            fieldAlwaysEdible.setAccessible(true);
+        } catch (Exception e) {
+            BrethitMod.LOGGER.warn("Mantle food hack failed!");
+            e.printStackTrace();
+            mantleLoaded = false;
+        }
+        for (Item item : ForgeRegistries.ITEMS) {
+            if (mantleLoaded && classItemEdible.isAssignableFrom(item.getClass())) {
+                try {
+                    BitSet alwaysEdible = (BitSet)fieldAlwaysEdible.get(item);
+                    alwaysEdible.set(0, 16, true);
+                } catch (Exception e) {
+                    BrethitMod.LOGGER.warn("Mantle food hack failed for " + item.getRegistryName());
+                    e.printStackTrace();
+                }
+            } else if (item instanceof ItemFood) {
+                ((ItemFood)item).setAlwaysEdible();
+            }
+        }
     }
 
     public void sendToast(LangConst.LangStr msg) {
